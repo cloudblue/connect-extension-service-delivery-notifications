3b8a16628b4610d2795ad99ac7204435621512c6
Paulina Marcinowska <paulina.marcinowska@cloudblue.com>
2022-12-23 16:10:34 +0100

    LITE-25996 index.js refactoring.

--- cen/static/js/index.js
+++ cen/static/js/index.js
@@ -1,139 +1,161 @@
 import createApp from '/static/js/toolkit.js';
-const app = createApp({});
+const createdApp = createApp({});
 
-app.listen('$init', main);
+createdApp.listen('$init', () => main(createdApp));
 $('#resend').click(resend);
+
+const dataTableConfig = {
+    'paging': true,
+    'searching': true,
+    'lengthChange': false,
+    'info': false,
+    'serverSide': true,
+    'processing': true,
+    'buttons': [
+        'searchPanes'
+    ],
+    'dom': 't',
+    'ajax': populateDataTable,
+    'columns': [
+        { 'data': 'id' },
+        {
+            'data': 'product_logo', render: getProductLogo
+        },
+        { 'data': 'email_to' },
+        {
+            'data': 'date', render: getDate
+        },
+        {
+            'data': 'date', render: getVisibilityButton
+        },
+    ],
+};
 
 export async function getEmailLogsList(limit, offset, search) {
     const response = await fetch(`/api/email_tasks?search=${search}&limit=${limit}&offset=${offset}`);
     let body = await response.json();
+    document.getElementById("loader-container").style.display = 'none';
+    document.getElementById("settings").style.display = 'block';
     return body;
-}
+};
 
-async function main() {
-    let offset = 0;
-    let search = '';
-    document.getElementById("EmailLogsTable").style.display = 'inline';
-    var t = $('#task-list-table').DataTable({
-        'paging': false,
-        'searching': false,
-        'info': false,
-        'serverSide': true,
-        'processing': true,
-        'buttons': [
-            'searchPanes'
-        ],
-        'ajax': async function (data, callback, settings) {
-
-            const limit = $('#select-rows-per-page').val();
-            let res = await getEmailLogsList(limit, offset, $('#search-input').val());
-            let count = res.count;
-            res = { 'data': res.results };
-            if (offset <= 1) {
-                $('#page-down').addClass('disabled');
-            } else {
-                $('#page-down').removeClass('disabled');
-            }
-            let pageMax = parseFloat(offset) + parseFloat(limit) - 1;
-            if (pageMax >= count) {
-                $('#page-up').addClass('disabled');
-                pageMax = count;
-            } else {
-                $('#page-up').removeClass('disabled');
-            }
-
-            $('#page-total').html(count);
-            $('#page-min').html(parseFloat(offset)+1);
-            $('#page-max').html(pageMax);
-            callback(res);
-        },
-        'columns': [
-            { 'data': 'id' },
-            {
-                'data': 'product_logo', render: function (data, type, row, meta) {
-                    return `<div class="td-product"><img src="${data}" alt="Logo" height="22"><div>
-                        <div class="name">${row['product_name']}</div>
-                        <div class="id">${row['product_id']}</div>
-                    </div></div>`
-                }
-            },
-            { 'data': 'email_to' },
-            {
-                'data': 'date', render: function (data, type, row, meta) {
-                    return moment.utc(data).format('MMMM Do YYYY, h:mm:ss a');
-                }
-            },
-            {
-                'data': 'date', render: function (data, type, row, meta) {
-                    return `<button type="button" class="material-icons" data-mdb-toggle="modal" data-mdb-target="#mailDetailModal"
-            data-mdb-whatever="${row['id']}">visibility</button>`;
-                }
-            },
-        ],
-    });
-
-    $('#search-input').keyup(function () {
-        if ($(this).val().length==0 || $(this).val().length>3) {
-            t.ajax.reload();
-        }
-
-    });
-
-    $('#select-rows-per-page').change(function () {
-        t.ajax.reload();
-    });
-
-    $('#page-down').click(function () {
-        const limit = $('#select-rows-per-page').val();
-        offset = (parseFloat(offset) - parseFloat(limit));
-        if (offset < 0) offset = 0;
-        t.ajax.reload();
-    });
-
-    $('#page-up').click(function () {
-        const limit = $('#select-rows-per-page').val();
-        offset = parseFloat(offset) + parseFloat(limit);
-        t.ajax.reload();
-    });
+export function main(app) {
+    const table = createTable();
 
     const mailDetailModal = document.getElementById('mailDetailModal');
-    mailDetailModal.addEventListener('show.mdb.modal', async (e) => {
-        const button = e.relatedTarget;
-        const recipient = button.getAttribute('data-mdb-whatever');
-        const response = await fetch(`/api/email_task/${recipient}`);
-        const body = await response.json();
-        const dateEmail = moment.utc(body['date']).format('MMMM Do YYYY, h:mm:ss a');
+    mailDetailModal.addEventListener('show.mdb.modal', showMailDetailModal);
 
-        $('#task-id').html(recipient);
-        $('#email-from').html(body['email_from']);
-        $('#email-to').html(body['email_to']);
-        $('#date').html(dateEmail);
-        $('#product-id').html(body['product_id']);
-        $('#product-name').html(body['product_name']);
-        $('#asset-id').html(body['asset_id']);
-        $('#request-id').html(body['request_id']);
-        $('#email-content').html(body['body']);
-
-        const modalBody = mailDetailModal.getElementsByClassName('modal-dialog')[0];
-        const container = document.getElementById('container');
-
-        if (modalBody.offsetHeight >= container.offsetHeight) {
-            const height = modalBody.offsetHeight + 100;
-            app.emit('$size', { height });
-        }
-    });
-
-    mailDetailModal.addEventListener('hide.mdb.modal', async (e) => {
-        const container = document.getElementById('container');
-        const height = container.offsetHeight;
-        app.emit('$size', { height });
-        t.destroy();
-        main();
-    });
+    mailDetailModal.addEventListener('hide.mdb.modal', hideMailDetailModal(app, table));
     app.emit('$size', { height: 800 });
-}
+};
+
+export function hideMailDetailModal(app, table) {
+    return (e) => {
+        table.page( 'first' ).draw( 'page' );
+        app.emit('$size', { height: 800 });
+    };
+};
+
+export async function showMailDetailModal(e, app) {
+    const mailDetailModal = document.getElementById('mailDetailModal');
+    const button = e.relatedTarget;
+    const recipient = button.getAttribute('data-mdb-whatever');
+    const response = await fetch(`/api/email_task/${recipient}`);
+    const body = await response.json();
+    const dateEmail = moment.utc(body['date']).format('MMMM Do YYYY, h:mm:ss a');
+
+    $('#task-id').html(recipient);
+    $('#email-from').html(body['email_from']);
+    $('#email-to').html(body['email_to']);
+    $('#date').html(dateEmail);
+    $('#product-id').html(body['product_id']);
+    $('#product-name').html(body['product_name']);
+    $('#asset-id').html(body['asset_id']);
+    $('#request-id').html(body['request_id']);
+    $('#email-content').html(body['body']);
+
+    const modalBody = mailDetailModal.getElementsByClassName('modal-dialog')[0];
+    const container = document.getElementById('container');
+
+    if (modalBody.offsetHeight >= container.offsetHeight) {
+        const height = modalBody.offsetHeight + 100;
+        app.emit('$size', { height });
+    };
+};
+
+export async function populateDataTable(data, callback, settings) {
+    const limit = data.length;
+    const offset = data.start;
+    let res = await getEmailLogsList(limit, offset, $('#search-input').val());
+    let count = res.count;
+    res = { 'data': res.results, 'recordsTotal': count, 'recordsFiltered': count };
+    toggleDisabledClassOnPageDown(offset);
+    let pageMax = parseFloat(offset) + parseFloat(limit);
+    pageMax = toggleDisabledClassOnPageUp(pageMax, count);
+
+    $('#page-total').html(count);
+    $('#page-min').html(parseFloat(offset) + 1);
+    $('#page-max').html(pageMax);
+    callback(res);
+};
+
+export function toggleDisabledClassOnPageUp(pageMax, count) {
+    if (pageMax >= count) {
+        $('#page-up').addClass('disabled');
+        pageMax = count;
+    } else {
+        $('#page-up').removeClass('disabled');
+    }
+    return pageMax;
+};
+
+export function toggleDisabledClassOnPageDown(offset) {
+    if (offset <= 1) {
+        $('#page-down').addClass('disabled');
+    } else {
+        $('#page-down').removeClass('disabled');
+    };
+};
+
+export function createTable() {
+    const table = $('#task-list-table').DataTable(dataTableConfig);
+
+    $('#search-input').on('keyup', function () {
+        if ($(this).val().length > 3 || $(this).val() === '') {
+            table.ajax.reload();
+        };
+    });
+
+    $('#page-down').on('click', function () {
+        table.page( 'previous' ).draw( 'page' );
+    });
+
+    $('#page-up').on('click', function () {
+        table.page( 'next' ).draw( 'page' );
+    });
+
+    return table;
+};
+
+export function getProductLogo(data, type, row, meta) {
+    return `<div class="td-product"><img src="${data}" alt="Logo" height="22">
+                <div>
+                    <div class="name">${row['product_name']}</div>
+                    <div class="id">${row['product_id']}</div>
+                </div>
+            </div>`;
+};
+
+export function getDate(data, type, row, meta) {
+    return moment.utc(data).format('MMMM Do YYYY, h:mm:ss a');
+};
+
+export function getVisibilityButton(data, type, row, meta) {
+    return `<button type="button" class="material-icons" data-mdb-toggle="modal" data-mdb-target="#mailDetailModal"
+        data-mdb-whatever="${row['id']}">visibility</button>`;
+};
 
 export async function resend() {
     const id = $('#task-id').html();
     await fetch(`/api/tasks/${id}/resend`, { method: 'POST', });
-}
+};
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,15 +1,33 @@
 import { 
     getEmailLogsList,
+    main,
+    showMailDetailModal,
+    hideMailDetailModal,
+    populateDataTable,
+    toggleDisabledClassOnPageUp,
+    toggleDisabledClassOnPageDown,
+    createTable,
+    getProductLogo,
+    getVisibilityButton,
     resend,
+    getDate,
  } from '../cen/static/js/index.js';
 
+let expected;
+let app;
+const jquery = global.$;
 
 describe('getEmailLogsList', () => {
     beforeEach(() => {
-        global.fetch = jest.fn();
-        global.fetch.mockReturnValue(Promise.resolve({
-            json: () => Promise.resolve('OK')}));
+        jest.spyOn(global, 'fetch').mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve('OK')
+        }));
+        document.body.innerHTML = `
+            <div id="loader-container"></div>
+            <div id="settings"></div>
+        `;
     });
+
    it.each([
     ['limitValue', 'offsetValue', 'searchValue'],
     ['limitValue', 'offsetValue', undefined],
@@ -20,17 +38,279 @@ describe('getEmailLogsList', () => {
     });
 });
 
+describe('main', () => {
+    const getElementById = document.getElementById;
+    beforeEach(() => {
+        jest.spyOn(global, '$').mockReturnValue({
+            DataTable: () => ({title: 'table'}),
+            on: jest.fn()
+        });
+        app = {
+            emit: jest.fn()
+        };
+        jest.spyOn(document, 'getElementById').mockReturnValue({
+            addEventListener: jest.fn()
+        });
+        document.body.innerHTML = `
+            <div id="mailDetailModal"></div>`;
+
+        main(app);
+    });
+
+    afterEach(() => {
+        // to clear mocks from beforeEach
+        global.$ = jquery;
+        document.getElementById = getElementById;
+    });
+
+    it('should add event listener show.mdb.modal', () => {
+        expect(document.getElementById().addEventListener).toHaveBeenCalledWith('show.mdb.modal', expect.any(Function))
+    });
+    it('should add event listener hide.mdb.modal', () => {
+        expect(document.getElementById().addEventListener).toHaveBeenCalledWith('hide.mdb.modal', expect.any(Function))
+    });
+    it('should emit size with height set to 800', () => {
+        expect(app.emit).toHaveBeenCalledWith('$size', {"height": 800})
+    });
+});
+
+describe('hideMailDetailModal', () => {
+    let table;
+    const draw = jest.fn()
+    beforeEach(() => {
+        app = {
+            emit: jest.fn()
+        };
+        table = {
+            page: () => ({
+                draw
+            })
+        };
+
+        hideMailDetailModal(app, table)();
+    });
+
+    it('should call table.page.draw', () => {
+        expect(table.page().draw).toHaveBeenCalledWith('page')
+    });
+
+    it('should emit size with height set to 800', () => {
+        expect(app.emit).toHaveBeenCalledWith('$size', {"height": 800})
+    });
+});
+
+describe('showMailDetailModal', () => {
+    const event = {
+        relatedTarget: {
+            getAttribute: () => 'recipient'
+        }
+    };
+    const format = jest.fn();
+    beforeEach(() => {
+        jest.spyOn(global, '$').mockReturnValue({html: jest.fn()});
+        jest.spyOn(global, 'fetch').mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                email_from: 'email from',
+                email_to: 'email to',
+                product_id: 'product id',
+                product_name: 'product name',
+                asset_id: 'asset id',
+                request_id: 'request id',
+                body: 'email body'
+            })
+        }));
+        app = {
+            emit: jest.fn()
+        };
+        global.moment = {
+            utc: () => ({
+                format
+            })
+        };
+        document.body.innerHTML = `
+            <div id="mailDetailModal">
+                <div class="modal-dialog"></div>
+            </div>
+            <div id="container"></div>
+        `;
+
+        showMailDetailModal(event, app);
+    });
+    afterEach(() => {
+        global.$ = jquery;
+    });
+
+    it('should add recipient as content of #task-id', () => {
+        expect($('#task-id').html).toHaveBeenCalledWith('recipient')
+    });
+    it('should add email sender as content of #email-from', () => {
+        expect($('#email-from').html).toHaveBeenCalledWith('email from')
+    });
+    it('should add email recipient as content of #email-to', () => {
+        expect($('#email-to').html).toHaveBeenCalledWith('email to')
+    });
+    it('should call moment format', () => {
+        expect(global.moment.utc().format).toHaveBeenCalledWith('MMMM Do YYYY, h:mm:ss a');
+    });
+    it('should add product id as content of #product-id', () => {
+        expect($('#product-id').html).toHaveBeenCalledWith('product id')
+    });
+    it('should add product name as content of #product-name', () => {
+        expect($('#product-name').html).toHaveBeenCalledWith('product name')
+    });
+    it('should add asset id as content of #asset-id', () => {
+        expect($('#asset-id').html).toHaveBeenCalledWith('asset id')
+    });
+    it('should add request id as content of #request-id', () => {
+        expect($('#request-id').html).toHaveBeenCalledWith('request id')
+    });
+    it('should add email content as content of #email-content', () => {
+        expect($('#email-content').html).toHaveBeenCalledWith('email body')
+    });
+    it('should emit size with height set to 100', () => {
+        expect(app.emit).toHaveBeenCalledWith('$size', {"height": 100})
+    });
+});
+
+describe('populateDataTable', () => {
+    let callback;
+    const data = {
+        length: 10,
+        start: 0,
+    };
+
+    const res = {
+        data: 'results', 
+        recordsTotal: 12, 
+        recordsFiltered: 12
+    };
+
+    beforeEach(() => {
+        callback = jest.fn();
+        jest.spyOn(global, 'fetch').mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                results: 'results',
+                count: 12
+            })
+        }));
+
+        document.body.innerHTML = `
+            <div id="loader-container"></div>
+            <div id="settings"></div>
+        `;
+
+        populateDataTable(data, callback, '');
+    });
+
+    it('should populate DataTable', () => {
+        expect(callback).toHaveBeenCalledWith(res);
+    });
+});
+
+describe('toggleDisabledClassOnPageUp', () => {
+    beforeEach(() => {
+        document.body.innerHTML = `<div id="page-up"></div>`;
+    });
+
+    it('should add disabled class on page up', () => {
+        toggleDisabledClassOnPageUp(10, 9);
+        expect($('#page-up').attr('class')).toBe('disabled');
+    });
+
+    it('should remove disabled class on page up', () => {
+        toggleDisabledClassOnPageUp(10, 20);
+        expect($('#page-up').attr('class')).not.toBe('disabled');
+     });
+});
+
+describe('toggleDisabledClassOnPageDown', () => {
+    beforeEach(() => {
+        document.body.innerHTML = `<div id="page-down"></div>`;
+    });
+
+    it('should add disabled class on page down', () => {
+        toggleDisabledClassOnPageDown(0);
+        expect($('#page-down').attr('class')).toBe('disabled');
+    });
+
+    it('should remove disabled class on page down', () => {
+        toggleDisabledClassOnPageDown(10);
+        expect($('#page-down').attr('class')).not.toBe('disabled');
+    });
+});
+
+describe('createTable', () => {
+    let table;
+    beforeEach(() => {
+        jest.spyOn(global, '$').mockReturnValue({
+            DataTable: () => ({title: 'table'}),
+            on: jest.fn()
+        });
+
+        table = createTable();
+    });
+    afterEach(() => {
+        global.$ = jquery;
+    });
+
+    it('should return created table', () => {
+        expect(table).toEqual({title: 'table'});
+    });
+
+    it('should set event $.on 3 times', () => {
+        expect(global.$().on).toHaveBeenCalledTimes(3);
+    });
+});
+
+describe('getProductLogo', () => {
+    it('should return div with product logo', () => {
+        expected = `<div class="td-product"><img src="/some/path" alt="Logo" height="22">
+            <div>
+                <div class="name">some product name</div>
+                <div class="id">some id</div>
+            </div>
+        </div>`.replace(/ +</g, '<');
+
+        const productLogo = getProductLogo('/some/path', '', { product_name: 'some product name', product_id: 'some id' }, '').replace(/ +</g, '<');
+        expect(productLogo).toBe(expected);
+    });
+});
+
+describe('getDate', () => {
+    beforeEach(() => {
+        const format = jest.fn();
+        global.moment = {
+            utc: () => ({
+                format
+            })
+        };
+    });
+
+    it('should call moment format', () => {
+        getDate(1670875686);
+        expect(global.moment.utc().format).toHaveBeenCalledWith('MMMM Do YYYY, h:mm:ss a');
+    });
+});
+
+describe('getVisibilityButton', () => {
+    it('should return div with visibility button', () => {
+        expected = `<button type="button" class="material-icons" data-mdb-toggle="modal" data-mdb-target="#mailDetailModal"
+        data-mdb-whatever="some-id">visibility</button>`;
+
+        const visibilityButton = getVisibilityButton('', '', { id: 'some-id' }, '');
+        expect(visibilityButton).toBe(expected);
+    });
+});
+
 describe('resend', () => {
     const id = 123;
     beforeEach(() => {
-        global.$ = jest.fn();
-        global.$.mockReturnValue({
-            html: () => id
-        });
-        global.fetch = jest.fn();
-        global.fetch.mockReturnValue(Promise.resolve({
-            json: () => Promise.resolve('OK')}));
+        jest.spyOn(global, '$').mockReturnValue({html: () => id});
+        jest.spyOn(global, 'fetch').mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve('OK')
+        }));
     });
+
    it('should call fetch with correct url', async () => {
        await resend();
        expect(global.fetch).toHaveBeenCalledWith(`/api/tasks/${id}/resend`, { method: 'POST', });
